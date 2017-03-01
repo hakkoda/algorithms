af73723daa65c9ac2a2afcecd5aeb20e15e43cb2
Jason Tennant <xcodula@icloud.com>
2017-03-01 05:59:08 -0700

    adding comments to merge sort stuff

--- merge-sort.js
+++ merge-sort.js
@@ -13,8 +13,8 @@ Array.prototype.mergeSort = function(callback) {
 Array.prototype.mergeSortInner = function(lo, hi, callback) {
     if (hi <= lo) { return; }
     var mid = lo + Math.floor((hi - lo) / 2);
-    this.mergeSortInner(lo, mid);
-    this.mergeSortInner(mid+1, hi);
+    this.mergeSortInner(lo, mid, callback);
+    this.mergeSortInner(mid+1, hi, callback);
     this.merge(lo, mid, hi);
 };
 
@@ -25,13 +25,13 @@ Array.prototype.merge = function(lo, mid, hi, callback) {
         aux[k] = this[k];
     }
     for (let k = lo; k <= hi; k++) {
-        if (i > mid) { 
-            this[k] = aux[j++]; 
-        } else if (j > hi) { 
+        if (i > mid) {                              // Left half is exhausted, take from right 
+            this[k] = aux[j++];                    
+        } else if (j > hi) {                        // Right half is exhausted, take from left 
             this[k] = aux[i++]; 
-        } else if (aux.less(j, i, callback)) { 
+        } else if (aux.less(j, i, callback)) {      // Current key on the right is less than current key on left (take from the right) 
             this[k] = aux[j++]; 
-        } else { 
+        } else {                                    // Current key on the right is greater than (or equal) to current key on left (take from left) 
             this[k] = aux[i++]; 
         }
     }
